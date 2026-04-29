64ff2bb96815172979896fc69d1fc5eeaddedd69
laurymesa01 <mesafernandezlauraelena@gmail.com>
2026-04-29 10:34:19 -0700

    feat: Add initial structure for the Search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const Search = () => {
+  return (
+    <form action="">
+        <label for="search" >Buscar ciudad</label>
+        <input id="search" type="search" />
+        <button>Search</button>
+    </form>
+  )
+}
+
+export default Search
