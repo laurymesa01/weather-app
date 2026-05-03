d6f2b95e05a688401673797b090348014ccf96e0
laurymesa01 <mesafernandezlauraelena@gmail.com>
2026-05-03 10:59:59 -0700

    feat: Add initial structure for the Daily Forecast component

---
+++ src/components/DailyForecast.jsx
@@ -0,0 +1,61 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const DailyForecast = () => {
+  return (
+    <ul>
+        <li>
+            <p>Tue</p>
+            <img src="" alt="" />
+            <div>
+                <p>20</p>
+                <p>14</p>
+            </div>
+        </li>
+        <li>            
+            <p>Wed</p>
+            <img src="" alt="" />
+            <div>
+                <p>21</p>
+                <p>15</p>
+            </div></li>
+        <li>            
+            <p>Thu</p>
+            <img src="" alt="" />
+            <div>
+                <p>24</p>
+                <p>14</p>
+            </div></li>
+        <li>            
+            <p>Fri</p>
+            <img src="" alt="" />
+            <div>
+                <p>21</p>
+                <p>13</p>
+            </div></li>
+        <li>            
+            <p>Sat</p>
+            <img src="" alt="" />
+            <div>
+                <p>21</p>
+                <p>15</p>
+            </div></li>
+        <li>            
+            <p>Sun</p>
+            <img src="" alt="" />
+            <div>
+                <p>25</p>
+                <p>16</p>
+            </div></li>
+        <li>            
+            <p>Mon</p>
+            <img src="" alt="" />
+            <div>
+                <p>24</p>
+                <p>15</p>
+            </div></li>
+    </ul>
+  )
+}
+
+export default DailyForecast
