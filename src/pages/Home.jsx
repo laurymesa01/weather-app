86daf0a477985d132398fb42e5d5d1fb40bc15f2
laurymesa01 <mesafernandezlauraelena@gmail.com>
2026-05-04 15:09:46 -0700

    style: Add styles to Header component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,12 @@ import  Header  from "../components/Header";
 
 const Home = () => {
   return (
-    <>
+    <main className='h-screen w-screen bg-neutral-900 py-2 px-4 xl:px-12 xl:py-8'>
         <Header />
-        <main>
+        <section >
           <h1>How’s the sky looking today?</h1>
-        </main>
-    </>
+        </section>
+    </main>
 
   )
 }
