8a49232d4098f1fbd1613594eed2204e029bfbae
laurymesa01 <mesafernandezlauraelena@gmail.com>
2026-05-04 15:16:08 -0700

    style: Add styles to Home üage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,10 @@ import  Header  from "../components/Header";
 
 const Home = () => {
   return (
-    <main className='h-screen w-screen bg-neutral-900 py-2 px-4 xl:px-12 xl:py-8'>
+    <main className='h-screen w-screen bg-neutral-900 py-2 px-3 xl:px-12 xl:py-8'>
         <Header />
-        <section >
-          <h1>How’s the sky looking today?</h1>
+        <section className='mt-12'>
+          <h1 className='text-preset-2 text-neutral-0 text-center'>How’s the sky looking today?</h1>
         </section>
     </main>
 
