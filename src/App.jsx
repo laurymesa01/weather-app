321aa89a1178648b260c3750eee0bc41e5c098a2
laurymesa01 <mesafernandezlauraelena@gmail.com>
2026-05-06 12:05:37 -0700

    style: Add styles to Error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
+// eslint-disable-next-line no-unused-vars
 import Home from './pages/Home'
+import Error from './pages/Error'
+import  Header  from "./components/Header";
+
 
 function App() {
 
   return (
-    <Home/>
+    <main className='min-h-screen w-screen bg-neutral-900 py-2 px-3 xl:px-12 xl:py-8'>
+      <Header/>
+      <Error/>
+
+    </main>
+      
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import  Header  from "../components/Header";
 import Search from '../components/Search';
 import WeatherResults from '../components/WeatherResults';
 
 const Home = () => {
   return (
-    <main className='min-h-screen w-screen bg-neutral-900 py-2 px-3 xl:px-12 xl:py-8'>
-        <Header />
         <section className='mt-12'>
           <h1 className='text-preset-2 text-neutral-0 text-center'>How’s the sky looking today?</h1>
           <Search />
           <WeatherResults />
         </section>
-    </main>
 
   )
 }
