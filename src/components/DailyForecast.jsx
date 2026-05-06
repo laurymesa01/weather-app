df2b53678ed9bd4eec102fd98439e8f782eb5994
laurymesa01 <mesafernandezlauraelena@gmail.com>
2026-05-06 10:50:47 -0700

    style: Add styles to Daily Forecast component

--- src/components/DailyForecast.jsx
+++ src/components/DailyForecast.jsx
@@ -3,57 +3,58 @@ import React from 'react'
 
 const DailyForecast = () => {
   return (
-    <ul>
-        <li>
-            <p>Tue</p>
-            <img src="" alt="" />
-            <div>
-                <p>20</p>
-                <p>14</p>
+    <ul className='grid grid-cols-3 md:grid-cols-7 gap-4 mt-4'>
+        <li className='bg-neutral-800 rounded-xl p-2 border border-neutral-600 text-center'>
+            <p className='text-preset-6 text-neutral-0'>Tue</p>
+            
+            <div className='flex justify-between items-center mt-4'>
+                <p className='text-preset-7 text-neutral-0'>20</p>
+                <p className='text-preset-7 text-neutral-200'>14</p>
             </div>
         </li>
-        <li>            
-            <p>Wed</p>
-            <img src="" alt="" />
-            <div>
-                <p>21</p>
-                <p>15</p>
-            </div></li>
-        <li>            
-            <p>Thu</p>
-            <img src="" alt="" />
-            <div>
-                <p>24</p>
-                <p>14</p>
-            </div></li>
-        <li>            
-            <p>Fri</p>
-            <img src="" alt="" />
-            <div>
-                <p>21</p>
-                <p>13</p>
-            </div></li>
-        <li>            
-            <p>Sat</p>
-            <img src="" alt="" />
-            <div>
-                <p>21</p>
-                <p>15</p>
-            </div></li>
-        <li>            
-            <p>Sun</p>
-            <img src="" alt="" />
-            <div>
-                <p>25</p>
-                <p>16</p>
-            </div></li>
-        <li>            
-            <p>Mon</p>
-            <img src="" alt="" />
-            <div>
-                <p>24</p>
-                <p>15</p>
-            </div></li>
+        <li className='bg-neutral-800 rounded-xl p-2 border border-neutral-600 text-center'>            
+            <p className='text-preset-6 text-neutral-0'>Wed</p>
+            
+            <div className='flex justify-between items-center mt-4'>
+                <p className='text-preset-7 text-neutral-0'>21</p>
+                <p className='text-preset-7 text-neutral-200'>15</p>
+            </div>
+        </li>
+        <li className='bg-neutral-800 rounded-xl p-2 border border-neutral-600 text-center'>            
+            <p className='text-preset-6 text-neutral-0'>Thu</p>
+            <div className='flex justify-between items-center mt-4'>
+                <p className='text-preset-7 text-neutral-0'>24</p>
+                <p className='text-preset-7 text-neutral-200'>14</p>
+            </div>
+        </li>
+        <li className='bg-neutral-800 rounded-xl p-2 border border-neutral-600 text-center'>            
+            <p className='text-preset-6 text-neutral-0'>Fri</p>
+            <div className='flex justify-between items-center mt-4'>
+                <p className='text-preset-7 text-neutral-0'>21</p>
+                <p className='text-preset-7 text-neutral-200'>13</p>
+            </div>
+        </li>
+        <li className='bg-neutral-800 rounded-xl p-2 border border-neutral-600 text-center'>            
+            <p className='text-preset-6 text-neutral-0'>Sat</p>
+            <div className='flex justify-between items-center mt-4'>
+                <p className='text-preset-7 text-neutral-0'>21</p>
+                <p className='text-preset-7 text-neutral-200'>15</p>
+            </div>
+        </li>
+        <li className='bg-neutral-800 rounded-xl p-2 border border-neutral-600 text-center'>            
+            <p className='text-preset-6 text-neutral-0'>Sun</p>
+            <div className='flex justify-between items-center mt-4'>
+                <p className='text-preset-7 text-neutral-0'>25</p>
+                <p className='text-preset-7 text-neutral-200'>16</p>
+            </div>
+        </li>
+        <li className='bg-neutral-800 rounded-xl p-2 border border-neutral-600 text-center'>            
+            <p className='text-preset-6 text-neutral-0'>Mon</p>
+            <div className='flex justify-between items-center mt-4'>
+                <p className='text-preset-7 text-neutral-0'>24</p>
+                <p className='text-preset-7 text-neutral-200'>15</p>
+            </div>
+        </li>
     </ul>
   )
 }
